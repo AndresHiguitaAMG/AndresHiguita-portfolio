f5dbc90232c90d3d4a8832ab7831289c5385a1eb
Aved Music Group <andreshiguitaamg@gmail.com>
2025-07-02 11:56:27 -0500

    update

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,13 +1,5 @@
 export const Home = () => {
   return (
-//     <section className="h-screen flex items-center justify-center bg-black p-4">
-//         <div className="text-center">
-//     <h1 className="text-4xl font-bold text-gray-800">Edwin Andres Higuita</h1>
-//     <p className="text-lg text-gray-600 mt-2">Desarrollador Frontend especializado en React</p>
-//   </div>    
-//     </section>
-<h1 className="text-6xl text-red-500">Funciona Tailwind</h1>
-
-
+    <h1 className="text-6xl text-red-500">Funciona Tailwind</h1>
   )
 }
